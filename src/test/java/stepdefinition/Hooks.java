2e238a5a980956d380cdbf01b63b739a94fb7147
Adderly Mejía <addrmh@gmail.com>
2023-08-28 14:49:25 -0500

    Creating hooks with annotators

---
+++ src/test/java/stepdefinition/Hooks.java
@@ -0,0 +1,23 @@
+package stepdefinition;
+
+import io.cucumber.java.Before;
+
+import java.io.IOException;
+
+public class Hooks {
+
+    @Before("@FindPet")
+    public void beforeScenario() throws IOException {
+
+        StepDefinition sd = new StepDefinition();
+        if (StepDefinition.pet_id == null) {
+
+            sd.aPayloadFileNamed("addPet");
+            sd.userSendsARequestFromTheAPI("POST");
+            sd.theResponseStatusCodeIs(200);
+
+        }
+
+    }
+
+}

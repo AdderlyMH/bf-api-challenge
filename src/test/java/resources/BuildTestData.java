a350125a4d77604322fe37f7a8d4323d46482ec6
Adderly Mejía <addrmh@gmail.com>
2023-08-28 15:23:15 -0500

    Setting a string payload

--- src/test/java/resources/BuildTestData.java
+++ src/test/java/resources/BuildTestData.java
@@ -26,7 +26,7 @@ public class BuildTestData {
         Tag tag = new Tag();
         tag.setId(tags_id);
         tag.setName(tags_name);
-        Tag[] tagsArray = { tag };
+        Tag[] tagsArray = {tag};
 
         Pet pet = new Pet();
         pet.setId(id);
@@ -42,6 +42,29 @@ public class BuildTestData {
 
     public String addPetFile(String fileName) throws IOException {
         return new String(Files.readAllBytes(Paths.get("src/main/resources/" + fileName + ".json")));
+    }
+
+    public String updatePetPayload(String pet_id) {
+
+        return "{\n" +
+                "\t\"id\": " + pet_id + ",\n" +
+                "\t\"category\": {\n" +
+                "\t\t\"id\": 1,\n" +
+                "\t\t\"name\": \"string\"\n" +
+                "\t},\n" +
+                "\t\"name\": \"doggie\",\n" +
+                "\t\"photoUrls\": [\n" +
+                "\t\t\"string\"\n" +
+                "\t],\n" +
+                "\t\"tags\": [\n" +
+                "\t\t{\n" +
+                "\t\t\t\"id\": 7,\n" +
+                "\t\t\t\"name\": \"string\"\n" +
+                "\t\t}\n" +
+                "\t],\n" +
+                "\t\"status\": \"available\"\n" +
+                "}";
+
     }
 
 }
